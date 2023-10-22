9dcc3bcceb03dae686d78683c4dae98234b107dd
vigilante23 <50033762+vigilante23@users.noreply.github.com>
2023-10-22 16:21:20 +0530

    app.py

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, request
+import requests
+
+app = Flask(__name__)
+
+@app.route('/')
+def Homepage():
+    return render_template("index.html")
+
+@app.route('/weather', methods=['POST', 'GET'])
+def weather_data():
+    url = "https://api.openweathermap.org/data/2.5/weather"
+    param={
+        'city': request.form.get("city"),
+        'units': request.form.get("units"),
+        'appid': request.form.get('appid')
+    }
+    response =  requests.get(url, param)
+    data = response.json()
+    return f"data : {data}"
+
+if __name__ == '__main__':
+    app.run(host = "0.0.0.0", port  = 5002)
